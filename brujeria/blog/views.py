70e3a56cc706f5ca2e30cbc3cdc987f5f73398fd
varun <varun4563232@gmail.com>
2023-11-21 23:26:25 +0530

    Created templates -created base html file and extended it to other html files -added some basic css from bootstrap

--- brujeria/blog/views.py
+++ brujeria/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+post = [
+    {
+        'author': 'JK rowling',
+        'title': 'Harry potter',
+        'content': 'first post content',
+        'date_posted': 'August 12, 2023'
+    },
+    {
+        'author': 'William shakesphere',
+        'title': 'Merchant of venice',
+        'content': 'second post content',
+        'date_posted': 'August 12, 1999'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>You are at Blog-home</h1>')
+    context = {
+        'posts': post
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>You are at Blog-about</h1>')
+    return render(request,'blog/about.html',{'title': 'About'})
+
