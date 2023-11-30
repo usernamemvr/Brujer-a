5317e0c613d09f90a94a1fc592a3fb17e58e2d9c
varun <varun4563232@gmail.com>
2023-11-30 13:44:52 +0530

    Model was created and registered to the admin site data was connected to the template from database migrations were done

--- brujeria/blog/views.py
+++ brujeria/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-post = [
-    {
-        'author': 'JK rowling',
-        'title': 'Harry potter',
-        'content': 'first post content',
-        'date_posted': 'August 12, 2023'
-    },
-    {
-        'author': 'William shakesphere',
-        'title': 'Merchant of venice',
-        'content': 'second post content',
-        'date_posted': 'August 12, 1999'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
