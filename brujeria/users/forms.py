1557022f17116a71e25d445f60cb49bcaeba704b
varun <varun4563232@gmail.com>
2023-12-08 21:50:03 +0530

    Added profile updating features Added some default features like profile pictures made sure its repetitive for every new profile that is created

--- brujeria/users/forms.py
+++ brujeria/users/forms.py
@@ -1,12 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(label='First Name',widget=forms.TextInput)
-    last_name = forms.CharField(label='Last Name',widget=forms.TextInput)
-    DOB = forms.DateField()
+    name = forms.CharField(label='Name',widget=forms.TextInput)
+    DOB = forms.DateField(label='Date of Birth', widget=forms.SelectDateWidget(years=range(1900, 2023)))
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'DOB', 'email', 'password1', 'password2']
+        fields = ['username', 'name', 'DOB', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']        
