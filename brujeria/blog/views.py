0c7ba0f9d38829c210bb78c3df16004648f876f4
varun <varun4563232@gmail.com>
2023-12-18 22:11:08 +0530

    Finished the Project     - Added email password reset feature     - used environment variables     - Added pagination feature

--- brujeria/blog/views.py
+++ brujeria/blog/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import (
     ListView,
     DetailView,
@@ -5,7 +7,8 @@ from django.views.generic import (
     UpdateView,
     DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
 from .models import Post
 
 # def home(request):
@@ -19,6 +22,17 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+            user = get_object_or_404(User, username=self.kwargs.get('username'))
+            return Post.objects.filter(author = user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
